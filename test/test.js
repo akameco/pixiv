dadb7e0ff0add6e0d0d2e320e85a10bd1b8e05e2
akameco <akameco.t@gmail.com>
2016-04-01 21:34:56 +0900

    Add error test

--- test/test.js
+++ test/test.js
@@ -13,14 +13,44 @@ test('work', async t => {
 	t.same(json.title, 'test');
 });
 
+test('error pixiv.work has empty arguments', async t => {
+	const pixiv = new Pixiv(username, password);
+	try {
+		await pixiv.user();
+		t.fail('Exception is not thrown');
+	} catch (err) {
+		t.ok(err);
+	}
+});
+
 test('user', async t => {
 	const pixiv = new Pixiv(username, password);
 	const json = await pixiv.user(7076552);
 	t.same(json.account, 'akameco');
 });
 
+test('error pixiv.user has empty arguments', async t => {
+	const pixiv = new Pixiv(username, password);
+	try {
+		await pixiv.user();
+		t.fail('Exception is not thrown');
+	} catch (err) {
+		t.ok(err);
+	}
+});
+
 test('userWorks', async t => {
 	const pixiv = new Pixiv(username, password);
 	const json = await pixiv.userWorks(7076552);
 	t.true(typeof json === 'object');
+});
+
+test('error pixiv.user has empty arguments', async t => {
+	const pixiv = new Pixiv(username, password);
+	try {
+		await pixiv.user();
+		t.fail('Exception is not thrown');
+	} catch (err) {
+		t.ok(err);
+	}
 });
