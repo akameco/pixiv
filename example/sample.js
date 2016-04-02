a8c575a9bdb2e7b6e654e05b0c591af9823b2bfc
akameco <akameco.t@gmail.com>
2016-04-02 16:44:22 +0900

    Update example

--- example/sample.js
+++ example/sample.js
@@ -7,13 +7,17 @@ const password = process.env.PASSWORD;
 const pixiv = new Pixiv(username, password);
 
 pixiv.user(471355).then(info => {
-	console.log(info);
+	console.log(JSON.stringify(info, null, 2));
+});
+
+pixiv.work(56099861).then(info => {
+	console.log(JSON.stringify(info, null, 2));
 });
 
 pixiv.userWorks(471355).then(info => {
-	console.log(info);
+	console.log(JSON.stringify(info, null, 2));
 });
 
 pixiv.search('艦これ1000users入り', {mode: 'tag'}).then(res => {
-	console.log(res);
+	console.log(JSON.stringify(res, null, 2));
 });
