a245fe4e3adf57d2ef8a1f98b6a6fdc4a4d45da0
akameco <akameco.t@gmail.com>
2016-04-03 18:58:18 +0900

    Add download example

---
+++ example/download.js
@@ -0,0 +1,16 @@
+'use strict';
+const Pixiv = require('../');
+const pixivImg = require('pixiv-img');
+
+const username = process.env.USERNAME;
+const password = process.env.PASSWORD;
+
+const pixiv = new Pixiv(username, password);
+
+pixiv.search('艦これ10000users入り', {mode: 'tag'})
+	.then(json => {
+		const work = json.response[0];
+		console.log(`downloading... ${work.title} by ${work.user.name} `);
+		return pixivImg(work.image_urls.large);
+	}).then(() => console.log('finish!'))
+	.catch(console.log);
