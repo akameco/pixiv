acb93a6da2fe17be73d51cfd8a898b38ea294a96
akameco <akameco.t@gmail.com>
2016-08-13 19:54:17 +0900

    CleanUp

--- auth.js
+++ auth.js
@@ -9,55 +9,49 @@ class Auth {
 	}
 
 	_auth() {
-		return new Promise(resolve => {
-			if (this.headers && this.headers.Authorization) {
-				resolve();
-			}
+		if (this.headers && this.headers.Authorization) {
+			return Promise.resolve();
+		}
 
-			const body = {
-				client_id: 'bYGKuGVw91e0NMfPGp44euvGt59s',
-				client_secret: 'HP3RmkgAmEGro0gn1x9ioawQE8WMfvLXDz3ZqxpK',
-				grant_type: 'password',
-				username: this.username,
-				password: this.password
+		const body = {
+			client_id: 'bYGKuGVw91e0NMfPGp44euvGt59s',
+			client_secret: 'HP3RmkgAmEGro0gn1x9ioawQE8WMfvLXDz3ZqxpK',
+			grant_type: 'password',
+			username: this.username,
+			password: this.password
+		};
+
+		return got('https://oauth.secure.pixiv.net/auth/token', {body, json: true}).then(res => {
+			const token = res.body.response.access_token;
+			this.headers = {
+				Authorization: `Bearer ${token}`
 			};
-
-			got('https://oauth.secure.pixiv.net/auth/token', {body, json: true}).then(res => {
-				const token = res.body.response.access_token;
-				this.headers = {
-					Authorization: `Bearer ${token}`
-				};
-				resolve();
-			});
 		});
 	}
 
 	authRequest(target) {
-		return new Promise((resolve, reject) => {
-			this._auth().then(() => {
-				const opts = {headers: this.headers, json: true};
-				got(target, opts).then(res => {
-					resolve(res.body);
-				}).catch(reject);
-			});
-		});
+		return this._auth().then(() => {
+			const opts = {
+				headers: this.headers,
+				json: true
+			};
+
+			return got(target, opts);
+		}).then(res => res.body);
 	}
 
 	authGot(apiUrl, path, query, method) {
 		method = method || 'get';
-		return new Promise((resolve, reject) => {
-			this._auth().then(() => {
-				const opts = Object.assign({
-					headers: this.headers,
-					json: true
-				}, {query});
+		return this._auth().then(() => {
+			const opts = Object.assign({
+				headers: this.headers,
+				json: true
+			}, {query});
 
-				const reqestURL = url.resolve(apiUrl, path);
-				got[method](reqestURL, opts).then(res => {
-					resolve(res.body);
-				}).catch(reject);
-			});
-		});
+			const reqestURL = url.resolve(apiUrl, path);
+			return got[method](reqestURL, opts);
+		})
+		.then(res => res.body);
 	}
 }
 
